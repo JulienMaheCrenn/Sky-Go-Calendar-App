96236290503427de11c25388f7c1be1a74ad1c08
Julien <julien.mahecrenn@sky.uk>
2023-02-07 15:43:39 +0000

    removing storyboard and rendering hello world label

--- Sky Go iOS Calendar App/Sky Go iOS Calendar App/ViewController.swift
+++ Sky Go iOS Calendar App/Sky Go iOS Calendar App/ViewController.swift
@@ -1,19 +1,34 @@
-//
-//  ViewController.swift
-//  Sky Go iOS Calendar App
-//
-//  Created by Julien Mahe-Crenn on 07/02/2023.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let introlabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        setupLabel()
 
+    }
+    func setupLabel() {
+        view.addSubview(introlabel)
+        
+        introlabel.text = "Hello World"
+        introlabel.backgroundColor = .green
+        introlabel.textAlignment = .center
+        
+        introlabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            introlabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            introlabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            introlabel.widthAnchor.constraint(equalToConstant: 200),
+            introlabel.heightAnchor.constraint(equalToConstant: 50),
+            
+        ])
+        
+        
+    }
 
 }
 
